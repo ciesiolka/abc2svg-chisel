72522f88b6e4647c68c6408c5d2621e751804cad
jef <jef>
2019-07-12 16:39:38 +0000

    modules: equalbars: bad key when change at end of line - reported by Gerhard Schacherl

--- modules/equalbars.js
+++ modules/equalbars.js
@@ -59,7 +59,12 @@ abc2svg.equalbars = {
 			t = s.time
 		}
 	}
-	bars.push([s, s.time - t]);
+
+	// push the last bar if it is not the invisible bar after a key change
+	if (!s.invis || s.prev.type != C.KEY)
+		bars.push([s, s.time - t])
+	else
+		bars[bars.length - 1][0] = s	// replace the last bar
 	t = s.time
 	if (s.dur)
 		t += s.dur;
@@ -70,10 +75,10 @@ abc2svg.equalbars = {
 
 	// set the measure parameters
 	x = s2.type == C.GRACE ? s2.extra.x : s2.x;
-	d = this.equalbars_d
-	if (!d)
-		d = this.equalbars_d = x;	// offset first note/rest
+	if (this.equalbars_d < x)
+		this.equalbars_d = x		// new offset of the first note/rest
 
+	d = this.equalbars_d
 	w = (width - d) / (t - t0)		// width per time unit
 
 	// loop on the bars
