5775732400b1f1d28f41388ba26fb6e2fad5fe83
jef <jef>
2019-07-12 08:53:06 +0000

    core: fix: bad bar numbers when first repeat "[1" at start of line - reported by Gerhard Schacherl

--- core/parse.js
+++ core/parse.js
@@ -1193,8 +1193,6 @@ function new_bar() {
 
 	// check if repeat bar
 	if (c > '0' && c <= '9') {
-		if (bar_type.slice(-1) == '[')
-			bar_type = bar_type.slice(0, -1);
 		s.text = c
 		while (1) {
 			c = line.next_char()
@@ -1205,7 +1203,6 @@ function new_bar() {
 		s.rbstop = 2;
 		s.rbstart = 2
 	} else if (c == '"' && bar_type.slice(-1) == '[') {
-		bar_type = bar_type.slice(0, -1);
 		s.text = ""
 		while (1) {
 			c = line.next_char()
