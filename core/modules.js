04f80953616e741b96412ddbaabbd716818f5bc9
jef <jef>
2020-02-10 13:45:31 +0000

    core: modules: add %%jianpu

--- core/modules.js
+++ core/modules.js
@@ -37,6 +37,7 @@ abc2svg.modules = {
 	equalbars: { fn: 'equalbars-1.js' },
 		grid: { fn: 'grid-1.js' },
 		grid2: { fn: 'grid2-1.js' },
+	jianpu: { fn: 'jianpu-1.js' },
 		MIDI: { fn: 'MIDI-1.js' },
 	pageheight: { fn: 'page-1.js' },
 		percmap: { fn: 'perc-1.js' },
