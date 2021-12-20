52403f6929138045ac29fcdc2dabc05e5ba65741
jef <jef>
2021-12-20 07:51:50 +0000

    core: fix: bad measure numbering after M: when M:none at start of tune - reported by Hudson Lacerda

--- core/tune.js
+++ core/tune.js
@@ -456,6 +456,7 @@ Abc.prototype.set_bar_num = function() {
 			continue
 		switch (s.type) {
 		case C.METER:
+		    if (wmeasure != 1)		// if not M:none
 			bar_num += (s.time - bar_tim) / wmeasure
 			bar_tim = s.time
 			wmeasure = s.wmeasure
