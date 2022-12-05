c5504e9023acfbef3173e199f3f9a8f5e8b8cf17
jef <jef>
2022-12-05 16:06:43 +0000

    core: fix: lack of space for !p!/!f! with %%dynalign -1

--- core/deco.js
+++ core/deco.js
@@ -734,12 +734,10 @@ function d_upstaff(de) {
 		}
 	}
 
-	if (dd.func != 6) {			// if not d_pf
 		if (y >= stafft)
 			y_set(s.st, 1, x - dd.wl, w, y + dd.h)
 		else if (y <= staffb)
 			y_set(s.st, 0, x - dd.wl, w, y - dd.hd)
-	}
 
 	de.x = x;
 	de.y = y
