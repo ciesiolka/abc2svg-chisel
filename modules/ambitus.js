fe8c6dc1f66ac5581a5a392c975acbe3817126dd
jef <jef>
2023-08-17 09:41:04 +0000

    modules: ambitus: fix: lack of helper lines - reported by Hudson Lacerda

--- modules/ambitus.js
+++ modules/ambitus.js
@@ -102,6 +102,25 @@ abc2svg.ambitus = {
 	of(cmd, param)
     },
 
+    set_glue: function(of, w) {
+    var	v, s,
+	voice_tb = this.get_voice_tb()
+
+	of(w)
+	if (this.cfmt().ambitus) {
+		for (v = 0; v < voice_tb.length; v++) {
+			s = voice_tb[v].sym
+			while (!s.time && !s.clef_type)
+				s = s.next
+			if (s && s.nhd) {	// draw the helper lines if any
+				s.x -= 26
+				this.draw_hl(s)
+				s.x += 26
+			}
+		}
+	}
+    }, // set_glue()
+
     set_width: function(of, s) {
 	if (s.clef_type != undefined && s.nhd > 0) {
 		s.wl = 40;
@@ -115,6 +134,7 @@ abc2svg.ambitus = {
 	abc.draw_symbols = abc2svg.ambitus.draw_symbols.bind(abc, abc.draw_symbols);
 	abc.set_pitch = abc2svg.ambitus.set_pitch.bind(abc, abc.set_pitch);
 	abc.set_format = abc2svg.ambitus.set_fmt.bind(abc, abc.set_format);
+	abc.set_sym_glue = abc2svg.ambitus.set_glue.bind(abc, abc.set_sym_glue)
 	abc.set_width = abc2svg.ambitus.set_width.bind(abc, abc.set_width)
     }
 } // ambitus
